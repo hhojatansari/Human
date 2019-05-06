f5c1a5bc327b28d6d3b3b2b7c717712de37a7a27
Hosein Hojat Ansari <hhojatansari@gmail.com>
2019-05-06 17:38:15 +0430

    Big change :D

---
+++ anatomy/head.py
@@ -0,0 +1,10 @@
+class Head:
+
+    def __init__(self):
+        self.__shape = "ND_FACE_SHAPE"
+
+    def set_shape(self, shape):
+        self.__shape = shape
+
+    def get_shape(self):
+        return self.__shape
